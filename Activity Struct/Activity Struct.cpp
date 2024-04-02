378b63c3ab9acc7fd4180b669a5598b3d6c5749f
Irfansyah001 <syahirfan545@gmail.com>
2024-04-02 14:03:29 +0700

    menambahkan cout alamat

--- Activity Struct/Activity Struct.cpp
+++ Activity Struct/Activity Struct.cpp
@@ -23,6 +23,8 @@ int main()
 	cout << endl;
 	cout << "\n Nim : " << mhs.nim;
 	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+
 int main(
 {
     std::cout << "Hello World!\n";
